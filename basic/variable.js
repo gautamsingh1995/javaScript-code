b26d3c93a450f694ba76275dcbf0906a9bae0466
gautamsingh1995 <125817615+gautamsingh1995@users.noreply.github.com>
2023-08-03 18:48:31 +0000

    variable

---
+++ basic/variable.js
@@ -0,0 +1,63 @@
+x = 10;
+//y = 20;
+z = x + y;
+//console.log(z);
+
+console.log("Gautam Singh");
+var a = 10
+var b = 40
+var c = a + b
+console.log(c);
+ 
+var x, y;
+x = 5 + 6;
+//y = x * 10;
+//console.log(y);
+
+var x = 10;
+// Here x is 10
+
+{
+  var x = 2;
+// Here x is 2
+}
+console.log(x); // return 2
+
+// Variables defined with let cannot be Redeclared
+
+// Variables defined with let must be Declared before use
+
+// Variables defined with let have Block Scope
+//let y = 10;
+// Here x is 10
+
+{
+let y = 2;
+// Here x is 2
+console.log(y);
+}
+console.log(y);
+
+// const variable
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total);
+
+const accountId = 1000
+let accountEmail = "gautamsinghcpj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gorakhpur"
+
+//accountId = 2 // not allowed
+
+accountEmail = "gkp@gmail.com"
+accountPassword ="2121212121"
+accountCity = "Bhathat"
+console.log(accountEmail);
+console.log(accountId);
+
+console.log(accountId,accountEmail,accountPassword,accountCity);
+
+
+
